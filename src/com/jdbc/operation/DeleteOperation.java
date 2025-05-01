83f18d34a4192f829326b25208576f410eeb91e3
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-05-01 21:19:49 +0530

    Update DeleteOperation.java

--- src/com/jdbc/operation/DeleteOperation.java
+++ src/com/jdbc/operation/DeleteOperation.java
@@ -3,7 +3,7 @@ package com.jdbc.operation;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement; 
+import java.sql.Statement;  
 
 public class DeleteOperation {
 public static void main(String[] args) throws ClassNotFoundException, SQLException {
