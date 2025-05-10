4ccc4b9d463ca95f08a53f71c394a9e329e122d5
princekumarscse <pk88498650@gmail.com>
2025-05-10 20:50:58 +0530

    operation matadata

--- src/com/jdbc/operation/FetchOperation.java
+++ src/com/jdbc/operation/FetchOperation.java
@@ -4,13 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;   
-   
+import java.sql.Statement;
+
 public class FetchOperation {
 public static void main(String[] args) throws ClassNotFoundException, SQLException {
 	//step 1)Load the driver
 		Class.forName("com.mysql.cj.jdbc.Driver");
-	//step 2)Stablish a connection between Jdbc program and Database
+	//step 2)Establish a connection between Jdbc program and Database
 		String url="jdbc:mysql://localhost:3306/test";
 		Connection connection=DriverManager.getConnection(url,"root","khushi1404");
 	//step 3)Prepare the SQL statement
@@ -25,6 +25,16 @@ public static void main(String[] args) throws ClassNotFoundException, SQLExcepti
 				System.out.println(resultSet.getInt(1)+" "+resultSet.getString(2)+" "+resultSet.getString(3));
 			}
 		}
+	//ResultSet is not backward compatible once it reaches null it will not go back	
+//		System.out.println("******************");
+//		if(resultSet!=null) {
+//			while(resultSet.next()) {
+//				System.out.println(resultSet.getInt(1)+" "+resultSet.getString(2)+" "+resultSet.getString(3));
+//			}
+//		}
+	//Therefore if we try to print 3 row it will not go back and throw exception	
+//		resultSet.absolute(3);
+//		System.out.println(resultSet.getInt(1)+" "+resultSet.getString(2)+" "+resultSet.getString(3));
 	//step 7)Close the resources
 		connection.close();
 		statement.close();
