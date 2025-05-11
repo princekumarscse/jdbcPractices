355e92dc6c5dacd90ba891120b2dc27987a74a3c
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-05-11 21:51:10 +0530

    Update StatementTypes.java

--- src/com/jdbc/statement/types/StatementTypes.java
+++ src/com/jdbc/statement/types/StatementTypes.java
@@ -7,7 +7,7 @@ import com.jdbc.entity.Student;
 //3.CallableStatement(Will discuss after store procedure in mysql)
 public class StatementTypes {
 public static void main(String[] args) {
-Student stu=new Student();
+Student stu=new Student(); 
 stu.setRollNumber(112);
 stu.setName("Ashok1");
 stu.setCollege("IIT BHU");
