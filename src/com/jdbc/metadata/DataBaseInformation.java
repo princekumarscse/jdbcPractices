fb111279eb8097b4ca5dc2deb3ec589b51478c1d
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-05-13 21:04:38 +0530

    Update DataBaseInformation.java

--- src/com/jdbc/metadata/DataBaseInformation.java
+++ src/com/jdbc/metadata/DataBaseInformation.java
@@ -3,7 +3,7 @@ package com.jdbc.metadata;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
-
+ 
 import com.jdbc.operationV2.JdbcUtils;
 
 public class DataBaseInformation { 
