a30ac821216971810575eae15d936851556d55f6
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-05-13 21:05:01 +0530

    Update ResultSetInformation.java

--- src/com/jdbc/metadata/ResultSetInformation.java
+++ src/com/jdbc/metadata/ResultSetInformation.java
@@ -5,7 +5,7 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+ 
 import com.jdbc.operationV2.JdbcUtils;
 /*
  * getCatalogName
