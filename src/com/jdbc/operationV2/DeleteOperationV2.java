43c5b8285b6555bbd42f79a49ba59687f3d9c924
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-05-18 11:15:43 +0530

    Update DeleteOperationV2.java

--- src/com/jdbc/operationV2/DeleteOperationV2.java
+++ src/com/jdbc/operationV2/DeleteOperationV2.java
@@ -3,7 +3,7 @@ package com.jdbc.operationV2;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
- 
+  
 public class DeleteOperationV2 {
 public static void main(String[] args) throws SQLException {
 	String query="delete from student where roll_number=12";
