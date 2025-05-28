9058e13d394fe2be8f1c02e1387ae6b2074bf80f
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-05-28 22:59:17 +0530

    Update FetchOperationV2.java

--- src/com/jdbc/operationV2/FetchOperationV2.java
+++ src/com/jdbc/operationV2/FetchOperationV2.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+ 
 public class FetchOperationV2 {
 public static void main(String[] args) throws ClassNotFoundException, SQLException {
 	//step 1)Load the driver
