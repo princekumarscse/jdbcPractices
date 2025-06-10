523622f14cb5499d3e1ef25311647986d3625a5f
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-06-11 00:34:10 +0530

    Update FetchOperationV2.java

--- src/com/jdbc/operationV2/FetchOperationV2.java
+++ src/com/jdbc/operationV2/FetchOperationV2.java
@@ -3,7 +3,7 @@ package com.jdbc.operationV2;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.Statement; 
  
 public class FetchOperationV2 {
 public static void main(String[] args) throws ClassNotFoundException, SQLException {
