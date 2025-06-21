375416db509a8d89bb2a3b218b8393a60b2f355d
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-06-21 21:11:03 +0530

    Update InsertOperationV2.java

--- src/com/jdbc/operationV2/InsertOperationV2.java
+++ src/com/jdbc/operationV2/InsertOperationV2.java
@@ -3,7 +3,7 @@ package com.jdbc.operationV2;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+ 
 public class InsertOperationV2 {
 public static void main(String[] args) throws SQLException {
 	String query="insert into student values (1,'khushi','sirt')";
