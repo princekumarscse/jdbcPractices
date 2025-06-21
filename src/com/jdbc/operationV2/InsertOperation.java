3422d3152cca3f02e4a2d8883388f71d0bb8acbe
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-06-21 21:10:52 +0530

    Update InsertOperation.java

--- src/com/jdbc/operationV2/InsertOperation.java
+++ src/com/jdbc/operationV2/InsertOperation.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException; 
 import java.sql.Statement;
-
+ 
 public class insertOperation {
 public static void main(String[] args) throws SQLException {
 	//step 1)Load the driver
