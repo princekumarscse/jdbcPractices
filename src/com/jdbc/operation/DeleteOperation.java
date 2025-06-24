c9ed1c8494e72d92a1c4185e1422f9c99997418d
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-06-24 23:06:54 +0530

    Update DeleteOperation.java

--- src/com/jdbc/operation/DeleteOperation.java
+++ src/com/jdbc/operation/DeleteOperation.java
@@ -1,7 +1,7 @@
 package com.jdbc.operation;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.DriverManager; 
 import java.sql.SQLException;
 import java.sql.Statement;  
      
