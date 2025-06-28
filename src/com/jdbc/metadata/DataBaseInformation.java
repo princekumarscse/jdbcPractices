a70a4e768ceea469520beef4708d55297c6bf4db
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-06-28 21:19:40 +0530

    Update DataBaseInformation.java

--- src/com/jdbc/metadata/DataBaseInformation.java
+++ src/com/jdbc/metadata/DataBaseInformation.java
@@ -5,7 +5,7 @@ import java.sql.DatabaseMetaData;
 import java.sql.SQLException; 
    
 import com.jdbc.operationV2.JdbcUtils;
-
+ 
 public class DataBaseInformation { 
 public static void main(String[] args) throws SQLException {
 	Connection connection = JdbcUtils.getMysqlConnection();
