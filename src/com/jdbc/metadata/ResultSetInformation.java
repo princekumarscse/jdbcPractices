3f35046242b7e2de22209e6d1102b52ccc9c35c9
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-06-28 21:19:53 +0530

    Update ResultSetInformation.java

--- src/com/jdbc/metadata/ResultSetInformation.java
+++ src/com/jdbc/metadata/ResultSetInformation.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.Statement; 
  
 import com.jdbc.operationV2.JdbcUtils;
 /*
