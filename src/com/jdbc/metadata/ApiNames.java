62db99d5fa15807dcba58e0e3107c15a7025b5e7
princekumarscse <143896759+princekumarscse@users.noreply.github.com>
2025-08-15 00:16:11 +0530

    Update ApiNames.java

--- src/com/jdbc/metadata/ApiNames.java
+++ src/com/jdbc/metadata/ApiNames.java
@@ -5,7 +5,7 @@ import java.lang.reflect.Modifier;
 import java.util.Arrays;   
 import java.util.List;  
 import java.util.stream.Collectors; 
- 
+  
 public class ApiNames { 
 public static void main(String[] args) {
 	try {
